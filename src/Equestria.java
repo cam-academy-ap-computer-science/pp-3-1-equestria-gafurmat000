1174e5679dd71610d3d6efbfe4132f3bf395a52c
gafurmat000 <gafurmat000@CAM-LAB-14.bgsd.k12.wa.us>
2019-10-25 07:55:48 -0700

    one.

--- src/Equestria.java
+++ src/Equestria.java
@@ -1,9 +1,22 @@
 
+/* first identify what each coordinate is
+ * find the distance between each coordinate by
+ */
 public class Equestria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x1 = 31;
+		int y1 = 15;
+		int x2 = 35;
+		int y2 = 9;
+		//double 
+		//System.out.println("Distance between Baltimare and Manehatan:" + ")
 	}
+		
+		//public static double distance(double x1, double y1) {
+			//step 1 subtract x and y
+			//step 2 
+	
 
 }
+
